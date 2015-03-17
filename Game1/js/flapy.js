f0864e9fe04589537fffa2f94319d9d86fca9b54
MitkoP <mitko.vld.petrov@gmail.com>
2015-03-17 22:01:46 +0200

    1st Game

---
+++ Game1/js/flapy.js
@@ -0,0 +1,43 @@
+window.requestAnimFrame = (function(){
+	return  window.requestAnimationFrame       || 
+		window.webkitRequestAnimationFrame || 
+		window.mozRequestAnimationFrame    || 
+		window.oRequestAnimationFrame      || 
+		window.msRequestAnimationFrame   ||  
+		function( callback ){
+			return window.setTimeout(callback, 1000 / 60);
+		};
+})();
+
+window.cancelRequestAnimFrame = ( function() {
+	return window.cancelAnimationFrame          ||
+		window.webkitCancelRequestAnimationFrame    ||
+		window.mozCancelRequestAnimationFrame       ||
+		window.oCancelRequestAnimationFrame     ||
+		window.msCancelRequestAnimationFrame        ||
+		clearTimeout
+} )();
+
+
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var bird = {};
+var wall = {};
+var W = 400;
+var H = 600;
+var images = [];
+
+var rocket = new Object();
+
+
+
+
+
+
+
+
+
+
+
+
+
